bf88923f9d80250ba9252cb7d1cea41f9d56f3c3
yesolee <yesolee@kakao.com>
2025-01-05 19:32:26 +0900

    feedback

--- app/feedback.py
+++ app/feedback.py
@@ -25,16 +25,44 @@ db = client[database_name]  # 데이터베이스 이름
 songs_collection = db['songs']  # 컬렉션 이름
 analysis_collection =  db['analysis']
 
-# songs_collection.update_one(
-#     {"title": title},
-#     {"$inc": {"like_count": 1}}
-# )
+@app.get("/song/")
+def get_song_data(request:Request, emotion:str) :
+    # 감정에 해당하는 노래 중에서 랜덤으로 하나 선택
+    random_song = songs_collection.aggregate([
+        {"$match": {"emotion": emotion}},  # 감정에 맞는 노래만 필터링
+        {"$sample": {"size": 1}}  # 랜덤으로 1개의 노래 선택
+    ])
+    
+    # 결과가 있으면 반환
+    song = list(random_song)
+    
+    if song:
+        # 노래 정보 반환
+        selected_song = song[0]
+        song_data = {
+            "title": selected_song["title"],
+            "artist": selected_song["artist"],
+            "src": selected_song["src"]
+        }
 
-# analysis_collection.update_one(
-#     {"title": title},
-#     {"$inc": {"total_play_count": 1,
-#               f"emotion_summary.{emotion}": 1}}
-# )
+        songs_collection.update_one(
+            {"title": selected_song["title"]},
+            {"$inc": {"like_count": 1}}
+        )
+
+        analysis_collection.update_one(
+            {"title": selected_song["title"]},
+            {"$inc": {"total_play_count": 1,
+                      f"emotion_summary.{emotion}": 1}}
+        )
+
+        return templates.TemplateResponse("/feedback.html", {
+         "request": request,
+         "song": song_data
+    })
+    else:
+        return "알 수 없는 감정입니다."
+
 
 @app.post("/like")
 async def increase_like(data: dict = Body(...)):
